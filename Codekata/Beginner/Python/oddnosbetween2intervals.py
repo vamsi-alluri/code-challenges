96851f8c41aac837ac7154532def4d1b9e437efc
Alluri Vamsi <vamsi1306@gmail.com>
2019-04-09 01:32:48 +0530

    Update oddnosbetween2intervals.py

--- Codekata/Beginner/Python/oddnosbetween2intervals.py
+++ Codekata/Beginner/Python/oddnosbetween2intervals.py
@@ -2,8 +2,8 @@
 x,y = input().split()
 x = int(x)
 y = int(y)
-for i in range(x,y):
+for i in range(x+1,y):
      if(i%2==0):
          continue
      else:
-         print(i)
+         print(i,end=" ")
