c73e559c380937b2ad3b2888253fe56b35e1f9c7
Alluri Vamsi <vamsi1306@gmail.com>
2019-04-09 19:37:41 +0530

    Create SortAlternate.py

---
+++ Codekata/Hunter/Python/SortAlternate.py
@@ -0,0 +1,19 @@
+#Code is to be written here
+def alter(a, n): 
+    i = 0
+    j = n-1      
+    while (i < j):  
+      
+        print(arr[j], end =" ") 
+        j-= 1
+        print(arr[i], end =" ") 
+        i+= 1
+    if (n % 2 != 0): 
+        print(a[i],end="")
+        
+        
+n = int(input())
+arr = [int(x) for x in input().split()]
+arr.sort()
+print(arr)
+alter(arr,n)
