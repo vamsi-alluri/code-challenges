5f04308deacb244a0dcf99a2548ec55017ebfd70
Alluri Vamsi <vamsi1306@gmail.com>
2019-04-12 14:14:47 +0530

    Update sum(n)==palindrome.py

--- Codekata/Hunter/Python/sum(n)==palindrome.py
+++ Codekata/Hunter/Python/sum(n)==palindrome.py
@@ -1,9 +1,14 @@
-n = [int(s) for s in input()]
-r = []
-r=[str(sum(n))]
-s=r
-r.reverse()
-if (r==s):
+num=[int(x) for x in input()]
+temp =int(sum(num))
+x = temp
+su = 0
+while temp > 0:
+   digit = (temp % 10)
+   su += digit
+   su = su * 10
+   temp //= 10
+su = su//10
+if (x==su):
     print("YES")
 else:
     print("NO")
