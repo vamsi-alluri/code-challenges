056d57a715053ab31927e8c610d535b569afbe90
Alluri Vamsi <vamsi1306@gmail.com>
2019-04-12 14:25:31 +0530

    Create days.py

---
+++ Codekata/Player/Python/days.py
@@ -0,0 +1,15 @@
+day = input()
+if (day=='Monday'):
+    print("no")
+elif (day=='Tuesday'):
+    print("no")
+elif (day=='Wednesday'):
+    print("no")
+elif (day=='Thursday'):
+    print("no")
+elif (day=='Friday'):
+    print("no")
+elif (day=='Saturday'):
+    print("yes")
+elif (day=='Sunday'):
+    print("yes")
