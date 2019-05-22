79cb408c99bac632a68afd1953836586266b4368
Alluri Vamsi <vamsi1306@gmail.com>
2019-05-22 18:16:42 +0530

    Update dynamicMin(list).py

--- Codekata/Hunter/Python/dynamicMin(list).py
+++ Codekata/Hunter/Python/dynamicMin(list).py
@@ -1,7 +1,7 @@
+dummy = input()
 a = [int(x) for x in input().split()]
 solution = []
 l = len(a)
-for i in range(0,l):
-    print(a[0:i+1])
+for i in range(l):
     solution.append(str(min(a[0:i+1])))
 print(' '.join(solution))
