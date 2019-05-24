b58fcd0ae62c915d603520dfb425737ce63bfbef
Alluri Vamsi <vamsi1306@gmail.com>
2019-05-24 17:40:21 +0530

    Update listoflists(duplicates).py

--- Codekata/Hunter/Python/listoflists(duplicates).py
+++ Codekata/Hunter/Python/listoflists(duplicates).py
@@ -2,15 +2,15 @@ def sublist_duplicates(lst):
     sublists = []
 
     for item in set(lst):
-#        print(item)
-#        print(lst.count(item))
-#        sublists.append([item] * lst.count(item))
         if (lst.count(item)>=2):
             sublists.append(str(item))
         
-    return sublists
+    if not sublists:
+        print("unique")
+    else:
+        print(" ".join(sublists))
             
-#lst_of_duplicates = [1, 2, 10, 3, 4, 1, 's', 2, 3, 1, 4, 's']
+
 dummy = input()
 lst_of_duplicates = [int(x) for x in input().split()]
-print(' '.join(sublist_duplicates(lst_of_duplicates)))
+sublist_duplicates(lst_of_duplicates)
