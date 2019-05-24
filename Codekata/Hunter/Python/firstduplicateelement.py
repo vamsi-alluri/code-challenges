e86d7f3274ce5ff6e77370f713bd7e6cc84b7a89
Alluri Vamsi <vamsi1306@gmail.com>
2019-05-24 18:19:42 +0530

    Update firstduplicateelement.py

--- Codekata/Hunter/Python/firstduplicateelement.py
+++ Codekata/Hunter/Python/firstduplicateelement.py
@@ -3,6 +3,7 @@ def sublist_duplicates(lst):
     for item in set(lst):
         if (lst.count(item)>=2):
             return item
+    return "unique"
 dummy = input()
 lst_of_duplicates = [int(x) for x in input().split()]
 print(sublist_duplicates(lst_of_duplicates))
