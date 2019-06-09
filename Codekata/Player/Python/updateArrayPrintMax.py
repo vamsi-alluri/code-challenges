88dbb69a342e54fce7fb96d748dddb3b2164a912
Alluri Vamsi <vamsi1306@gmail.com>
2019-06-09 18:18:48 +0530

    Create updateArrayPrintMax.py

---
+++ Codekata/Player/Python/updateArrayPrintMax.py
@@ -0,0 +1,6 @@
+array = [int(x) for x in input().split()]
+second = [int(n) for n in input().split()]
+length_second = len(second)
+for i in range(0,length_second):
+    array.append(second[i])
+    print(max(array))
