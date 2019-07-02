6d38b37c2fb2f7ea7f864d79074d6530840258a5
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-02 11:06:31 +0530

    Create CheckSquare.py

---
+++ Codekata/Pro/Python/CheckSquare.py
@@ -0,0 +1,13 @@
+a = []
+b = []
+for i in range(4):
+    n = [int(i) for i in input().split()]
+    if i%2==0:
+        a.extend(n)
+    else:
+        b.extend(n)
+print(a,b)
+if ((abs(a[0]-a[1]) == abs(a[2]-a[3])) and (abs(b[0]-b[1]) == abs(b[2]-b[3]))):
+    print("yes")
+else:
+    print("no")
