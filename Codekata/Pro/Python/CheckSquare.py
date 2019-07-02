abada290e076c42c3356ab81272003eeb32089d5
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-02 11:16:07 +0530

    Update CheckSquare.py

--- Codekata/Pro/Python/CheckSquare.py
+++ Codekata/Pro/Python/CheckSquare.py
@@ -6,7 +6,6 @@ for i in range(4):
         a.extend(n)
     else:
         b.extend(n)
-print(a,b)
 if ((abs(a[0]-a[1]) == abs(a[2]-a[3])) and (abs(b[0]-b[1]) == abs(b[2]-b[3]))):
     print("yes")
 else:
