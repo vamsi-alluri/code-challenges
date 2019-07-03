a5ff60cef656f9a2bc07de72413689f9c343c064
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-03 11:06:28 +0530

    Create segmentTree(SumOfSubList).py

---
+++ Codekata/Pro/Python/segmentTree(SumOfSubList).py
@@ -0,0 +1,11 @@
+dummy,q = [int(x) for x in input().split()]
+n = [int(x) for x in input().split()]
+l = []
+for i in range(q):
+    s = [int(x) for x in input().split()]
+    l.append(s)
+for i in range(q):
+    lower = l[i][0]
+    upper = l[i][1]
+    s = sum(n[lower:upper+1])
+    print(s)
