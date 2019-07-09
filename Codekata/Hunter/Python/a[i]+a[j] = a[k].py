d594fa08966f6180d8a3d84dafbe566d3448f083
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-09 09:25:23 +0530

    Update a[i]+a[j] = a[k].py

--- Codekata/Hunter/Python/a[i]+a[j] = a[k].py
+++ Codekata/Hunter/Python/a[i]+a[j] = a[k].py
@@ -1,8 +1,9 @@
 from itertools import combinations
-n = [x for x in input().split()]
+dummy = input()
+n = [int(x) for x in input().split()]
 comb = combinations(n,3)
-n = list(sum(comp, ()))
+n = list(sum(comb, ()))
 l = len(n)
-for i in range(0,l,+3):
+for i in range(0,l,3):
     if ((n[i]+n[i+1])==n[i+2]):
         print(n[i],n[i+1],n[i+2])
