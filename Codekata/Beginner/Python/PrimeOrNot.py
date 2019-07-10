4387b68b2741dd8c38bf0b960a238eeef9d239ea
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-10 14:35:34 +0530

    Update PrimeOrNot.py

--- Codekata/Beginner/Python/PrimeOrNot.py
+++ Codekata/Beginner/Python/PrimeOrNot.py
@@ -1,17 +1,14 @@
 import math
 n = int(input())
-root_of_n = int(math.sqrt(n))
-if root_of_n==2:
-    print("yes")
-else:
-    f = 0
-    for i in range(2,root_of_n):
+f = 1
+if n>0:
+    for i in range(2,n//2):
         if n%i==0:
             f=0
             break
-        else:
-            f=1
-    if (f):
+    if f==1:
         print("yes")
     else:
         print("no")
+else:
+    print("no")
