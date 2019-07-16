66094de6e5cd358449d806cd21d28dcdab671682
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-16 14:46:32 +0530

    Update LargestNoPermutations.py

--- Codekata/Hunter/Python/LargestNoPermutations.py
+++ Codekata/Hunter/Python/LargestNoPermutations.py
@@ -1,12 +1,30 @@
-n = [*input()]
+b = []
+def toString(List): 
+    return ''.join(List) 
+
+def permute(a, l, r): 
+    if l == r: 
+        b.append(toString(a)) 
+    else: 
+        for i in range(l, r + 1): 
+            a[l], a[i] = a[i], a[l] 
+            permute(a, l + 1, r) 
+            a[l], a[i] = a[i], a[l] # backtrack 
+string = input()
+k = int(string)
 l = []
-for i in n:
+n = len(string) 
+a = list(string) 
+permute(a, 0, n-1)
+for i in b:
     l.append(int(i))
-l.sort(reverse=True)
-a = []
-for i in l:
-    a.append(str(i))
-if a == n:
+l.sort()
+if k == max(l):
     print("impossible")
 else:
-    print("".join(a))
+    for i in l:
+        if i<=k:
+            continue
+        else:
+            print(i)
+            break
