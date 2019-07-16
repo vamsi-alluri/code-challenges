72ba5961e4ab47f3b08a8b6411c586a75d6a84bd
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-16 14:45:29 +0530

    Update largestSubArray.py

--- Codekata/Hunter/Python/largestSubArray.py
+++ Codekata/Hunter/Python/largestSubArray.py
@@ -1,31 +1,31 @@
-b = []
-def toString(List): 
-    return ''.join(List)
-
-def permute(a, l, r): 
-    if l == r: 
-        b.append(toString(a)) 
-    else: 
-        for i in range(l, r + 1): 
-            a[l], a[i] = a[i], a[l] 
-            permute(a, l + 1, r) 
-            a[l], a[i] = a[i], a[l]
-            
-string = input()
-k = int(string)
-l = []
-n = len(string) 
-a = list(string) 
-permute(a, 0, n-1)
-for i in b:
-    l.append(int(i))
-l.sort()
-if k == max(l):
-    print("impossible")
+n = input()
+prev = n[0]
+previous = 0
+if ("a" and "b" not in n):
+    print(0)
 else:
-    for i in l:
-        if i<=k:
-            continue
-        else:
-            print(i)
-            break
+    c = 1
+    for i in n[1:]:
+        if prev == "a":
+            if i == "b":
+                c += 1
+            else:
+                if previous > c :
+                    pass
+                else:
+                    previous = c
+                c = 1
+        if prev == "b":
+            if i == "a":
+                c += 1
+            else:
+                if previous > c :
+                    pass
+                else:
+                    previous = c
+                c = 1
+        prev=i
+    if previous<c:
+        print(c)
+    else:
+        print(previous)
