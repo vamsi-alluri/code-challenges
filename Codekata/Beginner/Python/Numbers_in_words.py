e65b2324bcdd22650a0e7e1ac76bcf65e5f754b0
Alluri Vamsi <vamsi1306@gmail.com>
2019-07-19 20:37:47 +0530

    Create Numbers_in_words.py

---
+++ Codekata/Beginner/Python/Numbers_in_words.py
@@ -0,0 +1,24 @@
+n = input()
+single_digit = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "10": "Ten",
+    "11": "Eleven",
+    "12": "Twelve",
+    "13": "Thirteen",
+    "14": "Fourteen",
+    "15": "Fifteen",
+    "16": "Sixteen",
+    "17": "Seventeen",
+    "18": "Eighteen",
+    "19": "Nienteen",
+    "20": "Twenty"
+    }
+print(single_digit[n])
