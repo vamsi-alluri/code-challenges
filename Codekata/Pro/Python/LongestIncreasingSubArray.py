377c16b42045af4d8de5c94d16dfb02d2a333b2d
Alluri Vamsi <vamsi1306@gmail.com>
2019-08-13 20:50:49 +0530

    Create LongestIncreasingSubArray.py

---
+++ Codekata/Pro/Python/LongestIncreasingSubArray.py
@@ -0,0 +1,15 @@
+dummy = input()
+prev_count = 0
+n = [int(x) for x in input().split()]
+count = 0
+for i in range(1,len(n)):
+    if n[i-1]<n[i]:
+        count += 1
+    else:
+        if prev_count<count:
+            prev_count = count
+        count = 1
+if prev_count>count:
+    print(prev_count)
+else:
+    print(count)
