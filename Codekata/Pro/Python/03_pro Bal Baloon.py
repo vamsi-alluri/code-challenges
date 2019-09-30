f89217135945a8951351affb379f5aa8ada68efc
Alluri Vamsi <vamsi1306@gmail.com>
2019-10-01 00:11:32 +0530

    Update 03_pro Bal Baloon.py

--- Codekata/Pro/Python/03_pro Bal Baloon.py
+++ Codekata/Pro/Python/03_pro Bal Baloon.py
@@ -1,7 +1,10 @@
 def func(l):
     n = 2
     c = -1
-    mini = len(l[0])
+    if len(l[0])<len(l[1]):
+        mini = len(l[0])
+    else:
+        mini = len(l[1])
     for i in range(mini):
         s = ""
         s = s + l[1][i]
@@ -15,10 +18,13 @@ n,s = map(str, input().split())
 l = []
 if n == s:
     print(0)
+elif len(n)<len(s):
+    len(set([*n]))>len(set([*s])) ? print(abs(len(n))):print(abs(len(n)))
 else:
     l.append(n)
     l.append(s)
     ans = func(l)
+    print(ans)
     if ans == -1:
         print(len(s))
     else:
